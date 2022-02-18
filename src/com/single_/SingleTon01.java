a89893bf077f7d8925ebc93053f818bef902f6b3
wanghao <774797233@qq.com>
2022-02-18 16:25:34 +0800

    idea commit

--- src/com/single_/SingleTon01.java
+++ src/com/single_/SingleTon01.java
@@ -1,10 +1,10 @@
 package com.single_;
 
 public class SingleTon01 {
-    private String name;
+    private String name1;
     private static SingleTon01 st= new SingleTon01("唯一对象");
     private SingleTon01(String name){
-        this.name=name;
+        this.name1=name;
     };
     public static SingleTon01 getInstance(){
         return st;
