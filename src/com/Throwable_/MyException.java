5f97344abc55f7c5d7823dfff8a505a29d64c753
wanghao <774797233@qq.com>
2022-03-04 10:10:50 +0800

    MyException Test

---
+++ src/com/Throwable_/MyException.java
@@ -0,0 +1,14 @@
+package com.Throwable_;
+
+/**
+ * @author wanghao
+ * @version 1.0
+ */
+public class MyException extends Exception{
+    public MyException() {
+    }
+
+    public MyException(String message) {
+        super(message);
+    }
+}
