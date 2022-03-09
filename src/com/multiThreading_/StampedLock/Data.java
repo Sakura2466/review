787f667b3a423e3dd9ea984415717aed38c52347
wanghao <774797233@qq.com>
2022-03-09 16:09:42 +0800

    Thread Test

---
+++ src/com/multiThreading_/StampedLock/Data.java
@@ -0,0 +1,35 @@
+package com.multiThreading_.StampedLock;
+
+import java.util.concurrent.locks.StampedLock;
+
+/**
+ * @author wanghao
+ * @version 1.0
+ */
+public class Data {
+    private String message;
+    private final StampedLock stampedLock = new StampedLock();
+    public String getMessage(){
+        long rl = stampedLock.readLock();
+        try {
+            Thread.sleep(100);
+            return message;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            stampedLock.unlockRead(rl);
+        }
+    }
+    public void setMessage(String message){
+        long wl = stampedLock.writeLock();
+        try {
+            Thread.sleep(100);
+            this.message=message;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            stampedLock.unlockWrite(wl);
+        }
+    }
+}
