81591773b04378ce97f1a4eb255b4fb13b274787
wanghao <774797233@qq.com>
2022-03-09 17:19:44 +0800

    Thread Test

---
+++ src/com/multiThreading_/ThreadState/TerminatedState.java
@@ -0,0 +1,34 @@
+package com.multiThreading_.ThreadState;
+
+/**
+ * @author wanghao
+ * @version 1.0
+ */
+public class TerminatedState {
+    public static void main(String[] args) {
+        Task3 task3 = new Task3();
+        Thread thread = new Thread(task3);
+        thread.start();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(thread.getState());
+    }
+}
+
+class Task3 implements Runnable{
+    @Override
+    public void run() {
+        synchronized (this){
+            System.out.println(Thread.currentThread().getState());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getState());
+        }
+    }
+}
