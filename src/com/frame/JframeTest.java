8010d1764b9c40ef9bc6bb82deb7a29f96244648
wanghao <774797233@qq.com>
2022-04-01 18:08:30 +0800

    DataType Test

--- src/com/frame/JframeTest.java
+++ src/com/frame/JframeTest.java
@@ -1,16 +1,25 @@
 package com.frame;
 
-import javax.swing.*;
-
 /**
  * @author wanghao
  * @version 1.0
  */
 public class JframeTest {
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame();
-        jFrame.setSize(600, 600);
-        jFrame.setVisible(true);
-        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    public static final int INT = 1000000000;
+
+
+    public static void main(String[] args){
+        Long num = 9898989881231123L;
+        int count=0;
+        while(num!=0){
+            num/=10;
+            count++;
+        }
+        System.out.println("它是个"+count+"位的数！");
+        byte a = 2;
+        short b = 3;
+        char c = 'a';
+        System.out.println(a+b+c);
+        System.out.println((char)197);
     }
 }
